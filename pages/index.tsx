2414ba858d372be9e4b1e29dd7f9ebf9efedcbdd
genewang <genewang7@gmail.com>
2022-10-25 10:50:26 +0800

    feat- index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,24 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import LoginPage from './login'
+import { getCookie, getCookies } from "typescript-cookie";
+import CategoryPage from './category'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (getCookies() === null || getCookie('email') == undefined || getCookie('email') == null)
+      router.push('/login');
+    else
+      router.push('/category');
+  }, [])
+
   return (
-    <div>
-     <h1>This is Home Page</h1> 
-    </div>
+    <div></div>
   )
 }
 
